dc03de315eb61104a723c9883427cd02a688bf68
marcellmueller <mlmueller@protonmail.com>
2020-12-24 11:22:26 -0800

    Added react router, some basic routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,30 @@
+import { Route, NavLink, HashRouter } from 'react-router-dom';
 import './App.scss';
 import Nav from './Nav';
+import Login from './Login';
+import Create from './Create';
+import Home from './Home';
+
 export default function App() {
   return (
-    <div className="App">
-      <Nav></Nav>
-      <section className="main-container">
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/stuff">Stuff</a>
-        </li>
-        <li>
-          <a href="/contact">Contact</a>
-        </li>
-      </section>
-    </div>
+    <HashRouter>
+      <div className="App">
+        <Nav></Nav>
+        <section className="main-container">
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/login">Login</NavLink>
+          </li>
+          <li>
+            <NavLink to="/create">Create</NavLink>
+          </li>
+          <Route path="/" component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/create" component={Create} />
+        </section>
+      </div>
+    </HashRouter>
   );
 }
