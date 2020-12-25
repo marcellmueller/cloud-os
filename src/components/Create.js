c3ebb14c66a8dba32b90d32b595e8972dd5883b4
marcellmueller <mlmueller@protonmail.com>
2020-12-24 17:05:48 -0800

    Changed some form validation on create user component

--- src/components/Create.js
+++ src/components/Create.js
@@ -6,15 +6,27 @@ export default function Create() {
       <form className="login-form" method="POST" action="/login">
         <h2 id="login-title">Create User</h2>
         <label for="create-first-name">First Name:</label>
-        <input type="email" className="login-name" required name="email" />
+        <input type="text" className="login-name" required name="email" />
         <label for="create-last-name">Last Name:</label>
-        <input type="email" className="login-name" required name="email" />
+        <input type="text" className="login-name" required name="email" />
         <label for="login-email">Email:</label>
         <input type="email" id="login-email" required name="email" />
         <label for="login-password">Password:</label>
-        <input type="password" id="login-password" required name="email" />
+        <input
+          type="password"
+          minlength="6"
+          id="login-password"
+          required
+          name="email"
+        />
         <label for="login-password">Re-enter Password:</label>
-        <input type="password" id="login-password" required name="email" />
+        <input
+          type="password"
+          minlength="6"
+          id="login-password"
+          required
+          name="email"
+        />
         <button type="submit" id="login-button">
           Login
         </button>
