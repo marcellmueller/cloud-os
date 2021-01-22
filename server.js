4734b316cd0a03367a7a9a3053d2d5e83fddaa02
marcellmueller <mlmueller@protonmail.com>
2021-01-21 17:24:27 -0800

    Changed static folder to build to test deployment

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
-const bcrypt = require('bcrypt');
 const path = require('path');
 const app = express();
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'build')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(
